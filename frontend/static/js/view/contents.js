6dd441197e167d24af67b19d1e59d9470f5e65e8
D-w-nJ <jdu2121@ajou.ac.kr>
2022-05-05 01:23:21 +0900

    add contents
    파일 하나로 게시판 5개 만듦

---
+++ frontend/static/js/view/contents.js
@@ -0,0 +1,31 @@
+const $ = document;
+
+export default function MakeContents(root,kind,kindInKorean){
+    root.innerHTML = `<div id="${kind}Board"></div>`;
+    const board = $.getElementById(`${kind}Board`);
+
+    const boardName = $.createElement('div');
+    boardName.innerHTML = `${kindInKorean}게시판`;
+    boardName.className = "boardNameInvisual"
+
+    const register = $.createElement('div');
+    register.innerHTML = '등록하기';
+    register.className = `register${kind}`;
+
+    board.appendChild(boardName);
+    board.appendChild(register);
+    const listBox = $.createElement('div');
+    listBox.className = 'listBox';
+
+    for(var i=0; i<5; i++){
+        const listBar = $.createElement('div');
+        listBar.className = `listBar${i}`;
+        listBox.appendChild(listBar);  
+    }
+    root.appendChild(listBox);
+
+    const content = $.createElement('div');
+    content.className = 'content';
+    content.innerHTML=`${kindInKorean}`;
+    root.appendChild(content);
+}
